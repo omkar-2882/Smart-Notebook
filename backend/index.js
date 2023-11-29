70627a3067cea2794908a246b57c4d0841e04604
Omkar Gawde <omkar.gawde2002@gmail.com>
2023-11-29 14:21:33 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.use(cors())
 app.use(express.json())
 
 // Available Routes
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 
 // app.use(express.static(path.join(__dirname, "../build")));
